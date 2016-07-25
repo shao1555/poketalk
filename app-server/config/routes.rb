bc452a9341f6b59c078f0c7f34d5024c8e7b3df3
Sho Sawada <shao@1555.info>
2016-07-25 23:28:48 +0900

    それっぽくお化粧

--- app-server/config/routes.rb
+++ app-server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   resources :rooms, only: %i(index show) do
     resources :messages, only: %i(create)
