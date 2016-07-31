8b1ccd1df9ae4313b4b7247ff4bd87f8c50ff63f
Sho Sawada <shao@1555.info>
2016-08-01 02:35:36 +0900

    デプロイがらみ

--- app-server/app/controllers/application_controller.rb
+++ app-server/app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  force_ssl if: :use_ssl?
+
+  def use_ssl?
+    Rails.env.production? && request.env['HTTP_X_FORWARDED_FOR'].present?
+  end
+
   def current_user
     User.where(id: session[:current_user_id]).first
   end
