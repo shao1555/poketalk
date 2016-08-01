ddf502cc4bffb113daf64adc56d642498f6c4ddc
Sho Sawada <shao@1555.info>
2016-08-01 23:09:34 +0900

    ベースとなる OG

--- app-server/app/helpers/application_helper.rb
+++ app-server/app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+        site: 'PokéTalk - ポケトーク',
+        description: 'PokéTalk (ポケトーク) は近くにいるポケモンGoプレイヤー同士でチャットができるサービスです',
+        og: {
+          title: :title,
+          type: 'website',
+          image: '/og.jpg',
+          description: :description
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@poketalk_jp',
+          image: '/og.jpg',
+          title: :title,
+          description: :description
+        }
+    }
+  end
 end
