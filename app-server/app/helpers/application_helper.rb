72fdc1fefc3dbd3776e50deeb1e706382e7212ba
Sho Sawada <shao@1555.info>
2016-08-01 23:28:56 +0900

    meta タグを入れてみた

--- app-server/app/helpers/application_helper.rb
+++ app-server/app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
   def default_meta_tags
     {
-        site: 'PokéTalk - ポケトーク',
+        site: 'PokéTalk | ポケトーク',
+        reverse: true,
+        separator: '-',
         description: 'PokéTalk (ポケトーク) は近くにいるポケモンGoプレイヤー同士でチャットができるサービスです',
+        keywords: %w(ポケモン Pokemon ポケモンゴー ポケモンGO PokemonGo PokémonGO ポケモン攻略 ポケモンの巣 ポケモンチャット),
         og: {
           title: :title,
           type: 'website',
